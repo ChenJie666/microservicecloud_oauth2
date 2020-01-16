633c957ea324a622bca84651706743cdf583db43
ChenJie666 <792965772@qq.com>
2020-01-16 23:34:05 +0800

    注册中心设置账号密码，服务进行注册时需要带上账号密码！

--- microservicecloud-eureka-config-7001/src/main/java/com/hxr/springcloud/cfgbeans/WebSecurityConfigurer.java
+++ microservicecloud-eureka-config-7001/src/main/java/com/hxr/springcloud/cfgbeans/WebSecurityConfigurer.java
@@ -1,24 +1,37 @@
 package com.hxr.springcloud.cfgbeans;
 
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
-//
-//    /**
-//     * 访问配置中心需要输入账号密码，账号密码在配置文件中设置
-//     * @param http
-//     * @throws Exception
-//     */
-//    @Override
-//    public void configure(HttpSecurity http) throws Exception {
-//        http.csrf().disable().httpBasic();
-//    }
-//
-//
-//
-//}
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
+
+    /**
+     * 访问配置中心需要输入账号密码，账号密码在配置文件中设置
+     * @param http
+     * @throws Exception
+     */
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().httpBasic();  //TODO csrf是跨站请求伪造
+        http.authorizeRequests()
+                .antMatchers("/")
+                .hasRole("eureka_admin")
+                .and()
+                .formLogin();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("root")
+                .password("abc123")
+                .roles("eureka_admin");
+    }
+
+}
